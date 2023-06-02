6e6ed7446944613abc752b72cf72bc86d5121836
BesaMugi <132837073+BesaMugi@users.noreply.github.com>
2023-06-02 14:03:13 +0300

    Update index.js
    com

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const text = document.getElementById('text');
 const li = document.createElement('li');
 
 
-function fet () {fetch('https://api.chucknorris.io/jokes/random').then(res => res.json()).then(data => {li.textContent = data.value;
-text.appendChild(li);
-
+function fet () {fetch('https://api.chucknorris.io/jokes/random')
+  .then(res => res.json())
+  .then(data => {li.textContent = data.value;
+  text.appendChild(li);
 })}
 
 button.addEventListener("click", fet)
