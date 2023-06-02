88b5e7191ef2cce30d59cb8e5245ee36a4c23c50
BesaMugi <mugiwarabesa@mail.ru>
2023-06-02 09:52:44 +0300

    fetch

---
+++ index.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('button');
+const text = document.getElementById('text');
+const li = document.createElement('li');
+
+
+function fet () {fetch('https://api.chucknorris.io/jokes/random').then(res => res.json()).then(data => {li.textContent = data.value;
+text.appendChild(li);
+
+})}
+
+button.addEventListener("click", fet)
+
